495989bf614ae6882a1c44de5996782751c598a4
Rémi Blaise <pro@remi-blaise.com>
2020-10-14 08:00:00 +0200

    Code the app

---
+++ README.md
@@ -0,0 +1,25 @@
+# Sennder Movie App
+
+## Installation steps
+
+1) Install Python3
+
+2) Install Django
+
+```
+python3 -m pip install Django
+```
+
+3) Install dependencies
+
+```
+pip3 install requests
+```
+
+## How to run
+
+1) The `app` folder contains the application. Move into it: `cd app`
+
+2) Run the development server: `python3 manage.py runserver`
+
+3) Go to http://127.0.0.1:8000/movies/
---
+++ app/movies/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+
+
+def index(request):
+    movies = retrieveMoviesFromGhibliAPI()
+    context = {'movies': movies}
+    return render(request, 'movies/index.html', context)
+
+
+def retrieveMoviesFromGhibliAPI():
+    films = requests.get('https://ghibliapi.herokuapp.com/films/').json()
+    peopleList = requests.get('https://ghibliapi.herokuapp.com/people/').json()
+
+    for film in films:
+        film['people'] = []
+
+    for people in peopleList:
+        for film_url in people['films']:
+            next(filter(lambda film: film['url'] == film_url, films))['people'].append(people)
+
+    return films
